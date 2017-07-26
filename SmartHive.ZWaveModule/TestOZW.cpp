d6b7e3fe9ab76a3a5d7b092859ecbdba95ddfcb2
Maxim Khlupnov <m.khlupnov@gmail.com>
2017-07-26 16:06:42 +0300

    1

--- SmartHive.ZWaveModule/TestOZW.cpp
+++ SmartHive.ZWaveModule/TestOZW.cpp
@@ -1,9 +1,62 @@
 #include "TestOZW.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+/**open_zwave*/
+#include <Options.h>
+#include <Manager.h>
+#include <Defs.h>
+
+
+using namespace OpenZWave;
+
+bool temp = false;
+
+
+static uint32 g_homeId = 0;
+static bool   g_initFailed = false;
+
+typedef struct
+{
+	uint32			m_homeId;
+	uint8			m_nodeId;
+	bool			m_polled;
+	list<ValueID>	m_values;
+}NodeInfo;
+
+static list<NodeInfo*> g_nodes;
+static pthread_mutex_t g_criticalSection;
+static pthread_cond_t  initCond = PTHREAD_COND_INITIALIZER;
+static pthread_mutex_t initMutex = PTHREAD_MUTEX_INITIALIZER;
 
 int main()
 {
-	printf("hello from LinuxApplication 123!\n");
+	pthread_mutexattr_t mutexattr;
+
+	pthread_mutexattr_init(&mutexattr);
+	pthread_mutexattr_settype(&mutexattr, PTHREAD_MUTEX_RECURSIVE);
+	pthread_mutex_init(&g_criticalSection, &mutexattr);
+	pthread_mutexattr_destroy(&mutexattr);
+
+	pthread_mutex_lock(&initMutex);
+	printf("Starting MinOZW with OpenZWave Version %s\n");
+
+	printf("Starting MinOZW with OpenZWave Version %s\n", Manager::getVersionAsString().c_str());
+
+	// Create the OpenZWave Manager.
+	// The first argument is the path to the config files (where the manufacturer_specific.xml file is located
+	// The second argument is the path for saved Z-Wave network state and the log file.  If you leave it NULL 
+	// the log file will appear in the program's working directory.
+/*	Options::Create("../../../config/", "", "");
+	Options::Get()->AddOptionInt("SaveLogLevel", LogLevel_Detail);
+	Options::Get()->AddOptionInt("QueueLogLevel", LogLevel_Debug);
+	Options::Get()->AddOptionInt("DumpTrigger", LogLevel_Error);
+	Options::Get()->AddOptionInt("PollInterval", 500);
+	Options::Get()->AddOptionBool("IntervalBetweenPolls", true);
+	Options::Get()->AddOptionBool("ValidateValueChanges", true);
+	Options::Get()->Lock();
+
+	Manager::Create();*/
 	return 0;
 }
 
